3c04421d615126e70c88dd2c3888b1a9d30d13ee
Cynthia Iradukunda <37863089+ciradu2204@users.noreply.github.com>
2022-05-19 10:19:16 +0200

    Time: 0 ms (100.00%), Space: 55.1 MB (8.32%) - LeetHub

--- 704-binary-search/704-binary-search.java
+++ 704-binary-search/704-binary-search.java
@@ -2,6 +2,25 @@ class Solution {
     public int search(int[] nums, int target) {
         
       return recursive(nums, target, 0, nums.length-1);
+        
+       //iterative approach 
+      /**
+      int start = 0; 
+      int end = nums.length -1; 
+      while(start < end ){
+      
+         int mid = (start +end)/2
+         
+         if(nums[mid] == target){
+           return mid; 
+         }else if(nums[mid] <target){
+            start = mid + 1; 
+         }else {
+            end = mid -1; 
+         }
+      }
+      return -1; 
+      */
             
     }
     
@@ -24,11 +43,9 @@ class Solution {
         
       return  recursive(nums, target, mid+1, end);  
       
-    
+
           
     }
     
-    //testing          
-    // [-1, 0, 5]
-    // [-1], 0, [5]
+   
 }
