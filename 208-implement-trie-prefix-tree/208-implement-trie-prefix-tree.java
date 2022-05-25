500ad19a72d937689c6f3d447dd4073445ce90df
Cynthia Iradukunda <37863089+ciradu2204@users.noreply.github.com>
2022-05-25 13:18:14 +0200

    Time: 47 ms (75.28%), Space: 53.8 MB (78.88%) - LeetHub

--- 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.java
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.java
@@ -14,16 +14,17 @@ class Trie {
 //         Node currentCharacter = node;       
 //         for(int i=0; i<word.length(); i++){
 
-//              if(node.children.containsKey(word.charAt(i))){
-//                 currentCharacter  = node.children.get(word.charAt(i)); 
+//              if(currentCharacter.children.containsKey(word.charAt(i))){
+//                 currentCharacter  = currentCharacter.children.get(word.charAt(i)); 
 //              }else{
-//                 node.children.put(word.charAt(i), new Node());
+//                 currentCharacter.children.put(word.charAt(i), new Node());
 
 //             }  
                 
 //         }
         
 //         currentCharacter.isWord = true;      
+   
         
         Node currentNode = node;
         for (int i = 0; i < word.length(); i++) {
@@ -33,45 +34,41 @@ class Trie {
             }
             currentNode = currentNode.children.get(currentChar);
         }
-        currentNode.isWord = true; 
+        currentNode.isWord = true;
         
     }
     
     public boolean search(String word) {
-//          Node currentCharacter = node; 
-//         for(int i= 0; i<word.length(); i++){
-//              if(currentCharacter.children.containsKey(word.charAt(i))){
-//                  currentCharacter =  currentCharacter.children.get(word.charAt(i)); 
-//              }else{
-//               return false; 
-//              }
-//         }
+         Node currentCharacter = node; 
+        for(int i= 0; i<word.length(); i++){
+             if(currentCharacter.children.containsKey(word.charAt(i))){
+                 currentCharacter =  currentCharacter.children.get(word.charAt(i)); 
+             }else{
+              return false; 
+             }
+        }
         
-//         return currentCharacter.isWord;
+        return currentCharacter.isWord;
         
-        Node currentCharacter = searchPrefix(word); 
-        return  currentCharacter != null && currentCharacter.isWord; 
+        
     }
     
     public boolean startsWith(String word) {
-        Node currentCharacter = searchPrefix(word); 
-        return  currentCharacter != null; 
+         Node currentCharacter = node; 
+        for(int i= 0; i<word.length(); i++){
+             if(currentCharacter.children.containsKey(word.charAt(i))){
+                 currentCharacter =  currentCharacter.children.get(word.charAt(i)); 
+             }else{
+              return false; 
+             }
+        }
+        
+        return true;
     
         
     }
     
-      private Node searchPrefix(String word) {
-        Node current = node;
-        for (int i = 0; i < word.length(); i++) {
-           char curLetter = word.charAt(i);
-           if (current.children.containsKey(curLetter)) {
-               current = current.children.get(curLetter);
-           } else {
-               return null;
-           }
-        }
-        return current;
-    }
+   
     
     
     class Node{
