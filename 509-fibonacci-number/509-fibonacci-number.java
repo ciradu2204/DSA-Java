5cd8637434a0a72fe83a0f02172016a550efb7b0
Cynthia Iradukunda <37863089+ciradu2204@users.noreply.github.com>
2022-06-10 16:32:06 +0200

    Time: 0 ms (100.00%), Space: 40.9 MB (37.55%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,7 +1,10 @@
 class Solution {
     public int fib(int n) {
-        
-        if(n == 0){
+    
+    /*
+     Recursive approach 
+     
+      if(n == 0){
          return 0; 
         }
         
@@ -9,8 +12,34 @@ class Solution {
             return 1; 
         }
         
-        return fib(n-1) + fib(n-2); 
+      return fib(n-1) + fib(n-2);
+    
+    */
+        
+    if(n == 0){
+        return 0; 
     }
+        
+    if(n == 1){
+        return 1; 
+    }
+        
+     int[] dp = new int[n]; 
+     dp[0] = 1; 
+     dp[1] = 1; 
+    
+     for(int i=2; i<n; i++){
+         dp[i] = dp[i-1] + dp[i-2]; 
+     }
+        
+        
+    return dp[n-1];
+        
+        
+    }
+    
+    
+    
 
      
 }
