31928096fd45c69eea69655a44e4c598458c39ca
Cynthia Iradukunda <37863089+ciradu2204@users.noreply.github.com>
2022-07-16 10:44:11 +0200

    Time: 0 ms (100.00%), Space: 42.1 MB (77.27%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,21 +1,46 @@
 class Solution {
     public void sortColors(int[] nums) {
         
-        int[] count = new int[3]; 
         
-        for(int i=0; i<nums.length; i++){
-            int currentElement = nums[i]; 
-             count[currentElement] +=1;  
-        }
+//         bucket sort or count sort
+//         int[] count = new int[3]; 
+        
+//         for(int i=0; i<nums.length; i++){
+//             int currentElement = nums[i]; 
+//              count[currentElement] +=1;  
+//         }
         
   
+//         int index = 0;
+//         for(int i=0; i<count.length; i++){
+//             int times = count[i]; 
+//             while(times > 0){
+//                nums[index++] = i; 
+//                times--; 
+//             }
+//         }
+        
+        // using two pointers 
+        int start =  0  ; 
+        int end = nums.length -1; 
         int index = 0;
-        for(int i=0; i<count.length; i++){
-            int times = count[i]; 
-            while(times > 0){
-               nums[index++] = i; 
-               times--; 
+        
+        while( index <=end &&  start <= end ){
+            
+            if(nums[index] == 0){
+                nums[index] = nums[start]; 
+                nums[start] =  0; 
+                start++; 
+                index++;
+            }else if(nums[index] == 2){
+                nums[index] = nums[end]; 
+                nums[end] = 2; 
+                end--; 
+                //we won't icrement the index because nums[end] could be a zero   
+            }else{
+                index++; 
             }
+            
         }
     
         
@@ -26,19 +51,10 @@ class Solution {
     Given an array:   [1,0, 2, 1, 1, 1]
     sort them in place 
     
-    1,0, 2, 1, 1, 1
-    p1            p2
-    
-    Alg: 
-    
-    1. when p1 > p2: swap 
-    2. if(p2 == 2){
-       decrement p2
-       }
-       
-    3. if(p1 == 0){
-        p1++
-       }
+    [1,0,2]
+     p1 
+       p2
+       i
      
     
     */
